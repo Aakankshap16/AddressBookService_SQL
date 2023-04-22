8da34b4da5da3e7ffb1e657d4a3d5b3793ac2c63
Aakankshap16 <aakankshap971@gmail.com>
2023-04-22 22:26:14 +0530

    Edit contact of person using their name

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -19,3 +19,7 @@ SELECT * FROM address_book /* retrive data */
 INSERT INTO address_book VALUES('Aakanksha','Pandey','Sector-7','Bhilai','CG','490006','908765323','aakan@gmail.com'),
 								('Divya','Mishra','Block2','Lucknow','UP','490566','90845723','divya@gmail.com'),
 								('Shivam','Tripathi','Area -8/11','Bhopal','MP','370006','903456323','shivam@gmail.com')
+
+UPDATE address_book /* Edit Contact */
+SET address = '456 New Lucknow', zip = '67890', phone_number = '5555678', email = 'divya612@gmail.com'
+WHERE first_name = 'Divya' AND last_name = 'Mishra';
